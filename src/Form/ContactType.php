d92a695a241ba18383cf918d227cd8451dc40afe
Laura Morlans <lauramb.morlans@gmail.com>
2018-10-16 14:26:09 +0200

    Seconde version

---
+++ src/Form/ContactType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('from', EmailType::class)
+            ->add('subject', TextType::class)
+            ->add('message', TextareaType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            //'data_class' => User::class
+        ));
+    }
+}
